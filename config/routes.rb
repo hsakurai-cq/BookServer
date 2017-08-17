fbbdf1fdd1f90aa9eb729949cea957c8e17061aa
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-17 11:47:21 +0000

    changed Auther name and email

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # scope :users do
-  #   post :signup, to: 'users#signup'
-  # end
   post '/signup', to: 'users#signup'
 end
