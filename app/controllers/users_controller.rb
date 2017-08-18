44daeaaa01869f323a370b37dc93b878ca2d034d
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-18 04:57:00 +0000

    login処理実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,16 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     user.token = SecureRandom.uuid
     if user.save
-      render json: {status: 200, result:user},status: :ok
+      render json: {status: 200, result: user},status: :ok
+    else
+      render json: {status: 'ERROR', message: 'User not saved', data: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      render json: {status: 200, result: user},status: :ok
     else
       render json: {status: 'ERROR', message: 'User not saved', data: user.errors}, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post '/signup', to: 'users#signup'
+  post '/login', to: 'users#login'
 end
