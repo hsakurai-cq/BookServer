46915b788d54baf2383bd81f4d3860921cb9df1a
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-21 07:17:26 +0000

    画像アップロード処理をImageHelperに分割

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,31 +1,14 @@
 require 'httpclient'
 
 class BooksController < ApplicationController
+  include ImageHelper
+
   before_action :authenticate_user!
 
   def create
-    time_stamp = Time.now.to_i.to_s
-    file_path = "public/img/#{time_stamp}.jpg"
-    image_data = Base64.urlsafe_decode64(params[:image_url])
-    File.open(file_path, 'wb') do |f|
-      #binding.pry
-      f.write(image_data)
-    end
-    http_client = HTTPClient.new
-    url = 'https://api.imgur.com/3/image'
-    auth_header = { 'Authorization' => 'Client-ID d285f83097c5323'}
-    File.open(file_path) do |file|
-      body = { 'image' => file }
-      @res = http_client.post(URI.parse(url), body, auth_header)
-    end
-    File.delete(file_path)
-    result_hash = JSON.load(@res.body)
-    p result_hash
-    puts result_hash['data']['link']
-
     book = Book.new(book_params)
     book.user_id = @current_user.id
-    book.image_url = result_hash['data']['link']
+    book.image_url = upload_image(Base64.urlsafe_decode64(params[:image_url]))
     if book.save
       render json: {status: 200, data: book},status: :ok
     else
---
+++ app/helpers/image_helper.rb
@@ -0,0 +1,28 @@
+require 'httpclient'
+
+module ImageHelper
+
+  def upload_image(encoded_image_string)
+    time_stamp = Time.now.to_i.to_s
+    file_path = "public/img/#{time_stamp}.jpg"
+    image_data = Base64.urlsafe_decode64(params[:image_url])
+    File.open(file_path, 'wb') do |f|
+      #binding.pry
+      f.write(image_data)
+    end
+
+    http_client = HTTPClient.new
+    url = 'https://api.imgur.com/3/image'
+    auth_header = { 'Authorization' => 'Client-ID d285f83097c5323'}
+    File.open(file_path) do |file|
+      body = { 'image' => file }
+      @res = http_client.post(URI.parse(url), body, auth_header)
+    end
+
+    File.delete(file_path)
+    result_hash = JSON.load(@res.body)
+    #p result_hash
+    return result_hash['data']['link']
+
+  end
+end
