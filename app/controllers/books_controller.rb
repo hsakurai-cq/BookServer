378a910ed7e396cfa80e0052f8744f362bbc407f
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-21 03:50:41 +0000

    DBに書籍情報を保存(user_id除く)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,33 +2,34 @@ require 'httpclient'
 
 class BooksController < ApplicationController
 
-  @@number = 0
-
   def create
-    file_path = "public/img/image_No.#{@@number}.jpg"
+    time_stamp = Time.now.to_i.to_s
+    file_path = "public/img/#{time_stamp}.jpg"
     image_data = Base64.urlsafe_decode64(params[:image_url])
-    #book.image_url = image_data
     File.open(file_path, 'wb') do |f|
       #binding.pry
       f.write(image_data)
     end
-
     http_client = HTTPClient.new
     url = 'https://api.imgur.com/3/image'
     auth_header = { 'Authorization' => 'Client-ID d285f83097c5323'}
     File.open(file_path) do |file|
       body = { 'image' => file }
       @res = http_client.post(URI.parse(url), body, auth_header)
-      @@number += 1
     end
-
     File.delete(file_path)
-
     result_hash = JSON.load(@res.body)
     p result_hash
-    result_hash['data']['link']
-  end
+    puts result_hash['data']['link']
 
+    book = Book.new(book_params)
+    book.image_url = result_hash['data']['link']
+    if book.save
+      render json: {status: 200, data: book},status: :ok
+    else
+      render json: {status: 'ERROR', message: 'Book not saved', data: book.errors}, status: :unprocessable_entity
+    end
+  end
 
   private
     def book_params
