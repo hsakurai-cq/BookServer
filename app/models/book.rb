c4f4387e9f0fbdbe4b8a8ce492daaf296eb7a4f7
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-21 09:39:38 +0000

    Book Model にvalidation追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :purchase_date, presence: true
+  validates :image_url, presence: true
 end
