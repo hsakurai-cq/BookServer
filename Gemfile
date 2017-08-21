b4ae6afae85fdb1fc91afcf6a10d512e25b8d354
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-21 03:19:45 +0000

    imgurに画像をアップロード

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'httpclient'
 gem 'therubyracer', platforms: :ruby
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
@@ -28,6 +29,7 @@ gem 'bcrypt', '~> 3.1.7'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
 end
 
 group :development do
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,32 @@
+require 'httpclient'
+
 class BooksController < ApplicationController
 
+  @@number = 0
+
   def create
-    book = Book.new(book_params)
-    book.purchase_date = DateTime.parse(params[:purchase_date])
-    if book.save
-      render json: {status: 200, result: book},status: :ok
-    else
-      render json: {status: 'ERROR', message: 'Book not saved', data: book.errors}, status: :unprocessable_entity
+    file_path = "public/img/image_No.#{@@number}.jpg"
+    image_data = Base64.urlsafe_decode64(params[:image_url])
+    #book.image_url = image_data
+    File.open(file_path, 'wb') do |f|
+      #binding.pry
+      f.write(image_data)
     end
+
+    http_client = HTTPClient.new
+    url = 'https://api.imgur.com/3/image'
+    auth_header = { 'Authorization' => 'Client-ID d285f83097c5323'}
+    File.open(file_path) do |file|
+      body = { 'image' => file }
+      @res = http_client.post(URI.parse(url), body, auth_header)
+      @@number += 1
+    end
+
+    File.delete(file_path)
+
+    result_hash = JSON.load(@res.body)
+    p result_hash
+    result_hash['data']['link']
   end
 
 
