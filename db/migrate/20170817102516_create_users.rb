95603b43f88215785b90c88cf773bf2134728140
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-23 02:17:52 +0000

    NotNull制約を追加

--- db/migrate/20170817102516_create_users.rb
+++ db/migrate/20170817102516_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password
-      t.string :token
+      t.string :email, :null => false
+      t.string :password, :null => false
+      t.string :token, :null => false
 
       t.timestamps
     end
--- db/migrate/20170818074217_create_books.rb
+++ db/migrate/20170818074217_create_books.rb
@@ -1,11 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.string :name
-      t.integer :price
-      t.datetime :purchase_date
-      t.string :image_url
-      t.integer :user_id
+      t.string :name, :null => false
+      t.integer :price, :null => false
+      t.datetime :purchase_date, :null => false
+      t.string :image_url, :null => false
+      t.integer :user_id, :null => false
 
       t.timestamps
     end
