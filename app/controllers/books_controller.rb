700021b65ad4af5111afbe95db81756f508bea90
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-23 09:16:06 +0000

    エラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,21 @@ class BooksController < ApplicationController
   include ImageHelper
   before_action :authenticate_user!
 
+  @@current_page = 1
+
   def index
-    if page = params[:page]
+    if page = params[:page].to_i
       #books = Book.where(user_id: @current_user.id).order(:created_at).limit(page.to_i)
-      books = @current_user.books.order(:created_at).limit(page.to_i)
-      render_json_succes(books)
+      books = @current_user.books.order(:created_at).limit(2).offset((page - 1) * 2)
+      render json: {code: 200,
+                      # result: BookSerializer.new(books, each_serializer: BookSerializer),
+                      result: ActiveModel::Serializer::CollectionSerializer.new(books, each_serializer: BookSerializer),
+                      total_count: 2 * page,
+                      total_pages: page,
+                      current_page: @@current_page,
+                      limit: 2 * page,
+                    },status: :ok
+      @@current_page += 1
     else
       render_json_failure("Wrong Paging")
     end
@@ -27,9 +37,9 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.image = upload_image(params[:image])
+    params[:image] = upload_image(params[:image])
     if book.update_attributes(book_params)
-      render_json_success(book)
+      render_book_action_success(book)
     else
       render_json_failure("Cannot update book")
     end
@@ -37,6 +47,6 @@ class BooksController < ApplicationController
 
   private
     def book_params
-      params.permit(:name, :price, :purchase_date, :image)
+      params.permit(:name, :price, :purchase_date, :image, :page)
     end
 end
