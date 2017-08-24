3bb7852bcd5293ef3624b1d5abd31b79545ba29e
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-24 02:19:47 +0000

    upload_imageメソッドを分割

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -3,25 +3,32 @@ require 'httpclient'
 module ImageHelper
 
   def upload_image(encoded_image_string)
-    time_stamp = Time.now.to_i.to_s
-    file_path = "public/img/#{time_stamp}.jpg"
-    image_data = Base64.urlsafe_decode64(encoded_image_string)
-    File.open(file_path, 'wb') do |f|
-      #binding.pry
-      f.write(image_data)
-    end
+    @@file_path = "public/img/#{Time.now.to_i.to_s}.jpg"
 
+    store_iamge_to_local(encoded_image_string)
+    request_imgur_url
+  end
+
+  def store_iamge_to_local(encoded_image_string)
+    File.open(@@file_path, 'wb') do |f|
+      #binding.pry
+      f.write(Base64.urlsafe_decode64(encoded_image_string))
+    end
+  end
+
+  def request_imgur_url
     http_client = HTTPClient.new
     url = 'https://api.imgur.com/3/image'
     auth_header = { 'Authorization' => 'Client-ID ' + ENV['IMGUR_CLIENT_ID']}
-    File.open(file_path) do |file|
+
+    File.open(@@file_path) do |file|
       body = { 'image' => file }
       @res = http_client.post(URI.parse(url), body, auth_header)
     end
 
-    File.delete(file_path)
+    File.delete(@@file_path)
     result_hash = JSON.load(@res.body)
     #p result_hash
-    return result_hash['data']['link']
+    result_hash['data']['link']
   end
 end
